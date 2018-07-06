c4d56bc40d2cb7f26a818a699bcaa14749b09cf4
urvaius <urvaius@gmail.com>
2018-07-06 16:02:45 -0700

    got bootstrap hooked up in the index.js file too for the button.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 /* eslint-env browser */
 import _ from 'lodash';
 import './css/style.css';
-import Icon from './image/oneicon.svg';
+import './css/bootstrap.min.css';
+import Icon from './image/images.jpg';
 import runCode from './code';
 
 
@@ -9,12 +10,12 @@ const body = document.getElementsByTagName('body')[0];
 function component() {
   const element = document.createElement('div');
   const btn = document.createElement('button');
-
-  element.innerHTML = _.join(['Hello', 'webpack', ' ']);
+  element.innerHTML = _.join(['Hello', 'webpack '], ' ');
   element.classList.add('hello');
+  btn.className = ('btn btn-primary');
   btn.innerHTML = 'click me an check the console';
   btn.onclick = runCode;
-
+  // element.innerHTML = 'will be jumbotron';
   // add an image to our existing div
   const myIcon = new Image();
   myIcon.src = Icon;
