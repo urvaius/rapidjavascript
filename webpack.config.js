5ab8da40d9b518a638a9ab43329d1a1fc63b65d4
urvaius <urvaius@gmail.com>
2018-07-06 15:10:21 -0700

    using index 1 for basic html since i'm not sure on index.js javascrypt and making code from this yet.

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,8 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: {
-    app: './src/index.js',
+    // app: './src/index.js',
+    app: './src/index1.js',
     code: './src/code.js',
   },
   output: {
@@ -35,7 +36,7 @@ module.exports = {
     new CleanWebpackPlugin(['dist']),
     new HtmlWebPackPlugin({
       // title: 'output management',
-      // template: './src/index1.html',
+      template: './src/index1.html',
     }),
     // new HtmlWebPackPlugin({tempate: './index.html'})
   ],
